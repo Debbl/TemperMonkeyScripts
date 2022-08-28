8932a84d71068417322c7a4744bc1c982008635c
Debbl <61053131+Debbl@users.noreply.github.com>
2022-08-28 17:36:53 +0800

    Update index.js

--- 01_隐藏腾讯课堂弹窗/index.js
+++ 01_隐藏腾讯课堂弹窗/index.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         隐藏腾讯课堂正在观看提示
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  利用CSS隐藏腾讯课堂正在观看提示*2022-07-26*
+// @version      0.2
+// @description  利用CSS隐藏腾讯课堂正在观看提示*2022-08-28*
 // @author       Debbl
 // @match        https://ke.qq.com/webcourse/*
-// @icon         https://ke.qq.com/favicon.ico
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ke.qq.com
 // @license      MIT
 // @grant        none
 // ==/UserScript==
-
+ 
+// about https://aiwan.run/
+ 
 (function () {
   "use strict";
   var styleEl = document.createElement("style");
   var textNode = document.createTextNode(
-    "div[class^='player-marquee'], .marquee {opacity: 0 !important;}"
+    "#video-container div:nth-of-type(2) {opacity: 0 !important;}"
   );
   styleEl.appendChild(textNode);
   document.querySelector("head").appendChild(styleEl);
