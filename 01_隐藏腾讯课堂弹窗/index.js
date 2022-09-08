2eaf6718d36093fff8f8e8c4a78e6eb71f2875f1
Debbl <me@aiwan.run>
2022-09-08 12:03:20 +0800

    0.3

--- 01_隐藏腾讯课堂弹窗/index.js
+++ 01_隐藏腾讯课堂弹窗/index.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         隐藏腾讯课堂正在观看提示
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  利用CSS隐藏腾讯课堂正在观看提示*2022-08-28*
+// @version      0.3
+// @description  利用CSS隐藏腾讯课堂正在观看提示*2022-09-08*
 // @author       Debbl
 // @match        https://ke.qq.com/webcourse/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ke.qq.com
 // @license      MIT
 // @grant        none
 // ==/UserScript==
- 
+
 // about https://aiwan.run/
- 
+
 (function () {
   "use strict";
   var styleEl = document.createElement("style");
   var textNode = document.createTextNode(
-    "#video-container div:nth-of-type(2) {opacity: 0 !important;}"
+    "#video-container div:nth-of-type(2), .copyright-marquee-tips-container, {opacity: 0 !important;}"
   );
   styleEl.appendChild(textNode);
   document.querySelector("head").appendChild(styleEl);
