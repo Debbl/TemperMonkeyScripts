fe898e123a1bb333cc29243e0f308fd49389a390
Debbl <me@aiwan.run>
2023-02-19 16:16:28 +0800

    chore: update description

--- 01_隐藏腾讯课堂弹窗/index.js
+++ 01_隐藏腾讯课堂弹窗/index.js
@@ -2,7 +2,7 @@
 // @name         隐藏腾讯课堂正在观看提示
 // @namespace    http://tampermonkey.net/
 // @version      0.4
-// @description  利用CSS隐藏腾讯课堂正在观看提示*2022-09-08*
+// @description  利用CSS隐藏腾讯课堂正在观看提示 *2022-09-08*
 // @author       Debbl
 // @match        https://ke.qq.com/webcourse/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ke.qq.com
--- 02_B站直播关闭看板娘/index.js
+++ 02_B站直播关闭看板娘/index.js
@@ -2,10 +2,11 @@
 // @name         B站直播关闭看板娘
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  B站直播关闭看板娘
+// @description  B站直播关闭看板娘，删除相关元素 *2023-02-19*
 // @author       Debbl
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @license      MIT
 // @grant        none
 // ==/UserScript==
 
